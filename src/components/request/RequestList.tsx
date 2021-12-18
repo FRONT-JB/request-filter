edf4d2a3da976075fa748ace159d8de37edea82c
전병길 <jeonbg@kakao.com>
2021-12-18 23:19:10 +0900

    feature : NotFound 추가

--- src/components/request/RequestList.tsx
+++ src/components/request/RequestList.tsx
@@ -4,12 +4,16 @@ import RequestItem from './RequestItem';
 
 const RequestList = () => {
   const requestList = useSelector(requestListSelector);
+  const isEmptyRequest = requestList.length === 0;
+
   return (
     <div className='request'>
       <ul className='request__list'>
-        {requestList.map((request) => (
-          <RequestItem key={request.id} request={request} />
-        ))}
+        {!isEmptyRequest &&
+          requestList.map((request) => <RequestItem key={request.id} request={request} />)}
+        {isEmptyRequest && (
+          <div className='request__notfound'>조건에 맞는 견적 요청이 없습니다.</div>
+        )}
       </ul>
     </div>
   );
