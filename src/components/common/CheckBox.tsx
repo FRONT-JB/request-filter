42c148b45e0fb76da60b46599db8270a4a4578ab
전병길 <jeonbg@kakao.com>
2021-12-19 17:42:52 +0900

    fix : Checkbox Component 수정

--- src/components/common/CheckBox.tsx
+++ src/components/common/CheckBox.tsx
@@ -13,14 +13,14 @@ const CheckBox = ({ id, label, className, checked, onChange }: Props) => {
   return (
     <div className={`check-box ${className || ''}`}>
       <input
-        className={`check-box__input`}
+        className='check-box__input'
         type='checkbox'
         id={id}
         checked={checked}
         name={label}
         onChange={onChange}
       />
-      <label className={`check-box__label`} htmlFor={id}>
+      <label className='check-box__label' htmlFor={id}>
         {label}
       </label>
     </div>
