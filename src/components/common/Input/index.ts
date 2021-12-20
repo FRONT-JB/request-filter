d6a400ad4d7d8acd29ee5914528164fa3a2d2e53
전병길 <jeonbg@kakao.com>
2021-12-20 20:21:52 +0900

    fix : Input 폴더구조 변경

---
+++ src/components/common/Input/index.ts
@@ -0,0 +1,3 @@
+export { default as SelectBox } from './SelectBox';
+export { default as CheckBox } from './CheckBox';
+export { default as Toggle } from './Toggle';
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,4 +1,2 @@
 export { default as Logo } from './Logo';
-export { default as SelectBox } from './SelectBox';
-export { default as CheckBox } from './CheckBox';
-export { default as Toggle } from './Toggle';
+export * from './Input';
